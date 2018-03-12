35eec8e33aae0e98183339a619aaf3ff1e3b4038
Leon <leon@Leons-MacBook-Pro.local>
2018-03-12 14:28:09 -0400

    removed ignored files

--- src/main/java/com/zipcodewilmington/person/generic/GenericPerson.java
+++ src/main/java/com/zipcodewilmington/person/generic/GenericPerson.java
@@ -6,14 +6,14 @@ import java.util.Date;
 /**
  * Created by leon on 3/8/18.
  */
-public class GenericPerson<T> {
+public class GenericPerson {
     public final Container<String> name;
     public final Container<Date> birthdate;
     public final Container<Color> eyeColor;
     public final Container<Integer> socialSecurityNumber;
 
     public GenericPerson(String name, Date birthdate, Color eyeColor, Integer socialSecurityNumber) {
-        this.name = new Container<>(name);
+        this.name = new Container<String>(name);
         this.birthdate = new Container<>(birthdate);
         this.eyeColor = new Container<>(eyeColor);
         this.socialSecurityNumber = new Container<>(socialSecurityNumber);
